05f80c8dbc9802717e606a097d5dbc9454994a68
Takashiro <takashiro@qq.com>
2013-02-11 13:11:09 +0800

    Name the new version Lugia
    Signed-off-by: Takashiro <takashiro@qq.com>

--- core/init.inc.php
+++ core/init.inc.php
@@ -1,7 +1,8 @@
-<?php
+﻿<?php
 
 define('IN_PLUSQUIZ', true);//用于防止模块文件被直接运行
 define('S_ROOT', dirname(dirname(__FILE__)).'/');//网站根目录常量
+define('S_VERSION', 'Lugia');
 error_reporting(E_ERROR | E_WARNING | E_PARSE);//Debug
 set_magic_quotes_runtime(0);//魔法括号
 set_time_limit(0);
